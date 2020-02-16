326ae121e80f57e4f6dcd4f42e8bb738ecda18d6
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-02-16 12:33:30 +0100

    Updated to latest Slimefun build

--- src/main/java/me/CHANGEME/slimefunaddon/ExampleAddon.java
+++ src/main/java/me/CHANGEME/slimefunaddon/ExampleAddon.java
@@ -5,6 +5,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
@@ -13,7 +14,7 @@ import me.mrCookieSlime.Slimefun.bstats.bukkit.Metrics;
 import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 
-public class SlimefunAddon extends JavaPlugin {
+public class ExampleAddon extends JavaPlugin implements SlimefunAddon {
 	
 	@Override
 	public void onEnable() {
@@ -56,12 +57,23 @@ public class SlimefunAddon extends JavaPlugin {
 		// RecipeType.ENHANCED_CRAFTING_TABLE refers to the machine in which this item is crafted in.
 		// Recipy Types from Slimefun itself will automatically add the recipe to that machine
 		SlimefunItem item = new SlimefunItem(category, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
-		item.register();
+		item.register(this);
 	}
 	
 	@Override
 	public void onDisable() {
 		// Logic for disabling the plugin...
+	}
+
+	@Override
+	public String getBugTrackerURL() {
+		// You can return a link to your Bug Tracker instead of null here
+		return null;
+	}
+
+	@Override
+	public JavaPlugin getJavaPlugin() {
+		return this;
 	}
 
 }
